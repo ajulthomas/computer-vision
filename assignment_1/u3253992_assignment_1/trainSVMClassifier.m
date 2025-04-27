505b0dc07a5faf177987de51ca5ee508d5cf5900
ajulthomas <ajul.thomas@outlook.com>
2025-04-27 23:02:28 +1000

    final test on exp 1

--- assignment_1/u3253992_assignment_1/trainSVMClassifier.m
+++ assignment_1/u3253992_assignment_1/trainSVMClassifier.m
@@ -1,7 +1,9 @@
 function SVMClassifier = trainSVMClassifier(trainingFeatures, trainingLabels)
-    t = templateSVM('KernelFunction', 'rbf', 'Standardize', true, 'Type','classification', 'Solver','SMO', 'KernelScale','auto');
-    options = struct('Optimizer', 'bayesopt', 'ShowPlots', true, 'UseParallel', true, 'CVPartition', cvpartition(trainingLabels, 'KFold', 5));
-    SVMClassifier = fitcecoc(trainingFeatures, trainingLabels, 'Learners', t, ...
-        'OptimizeHyperparameters', {'BoxConstraint'}, ...
-        'HyperparameterOptimizationOptions', options);
+    t = templateSVM('KernelFunction', 'gaussian', 'Standardize', true, 'Type','classification', 'Solver','SMO', 'KernelScale','auto'); 
+    SVMClassifier = fitcecoc(trainingFeatures, trainingLabels, 'Learners', t,'Coding','onevsall');
 end
+
+ % options = struct('Optimizer', 'bayesopt', 'ShowPlots', true, 'UseParallel', true);
+% 'OptimizeHyperparameters', {'BoxConstraint'}, ...
+% 'HyperparameterOptimizationOptions', options
+% 'BoxConstraint',0.93985
